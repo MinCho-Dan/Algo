a408d3355a32c4dfcf35b0b5f7185ffcda0d7a09
gyujik <safre2@naver.com>
2025-10-31 18:58:50 +0900

    maraton74 solve +1

--- algo/src/Maraton74/BJ_14928.java
+++ algo/src/Maraton74/BJ_14928.java
@@ -2,14 +2,21 @@ package Maraton74;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class BJ_14928 {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BigInteger num = new BigInteger(br.readLine());
-		System.out.println(num.mod(new BigInteger("20000303")).toString());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String numStr = br.readLine(); 
 
-	}
+        long remainder = 0;
+        int mod = 20000303;
+
+        for (int i = 0; i < numStr.length(); i++) {
+            int digit = numStr.charAt(i) - '0'; 
+            remainder = (remainder * 10 + digit) % mod;
+        }
+
+        System.out.println(remainder);
+    }
 }
