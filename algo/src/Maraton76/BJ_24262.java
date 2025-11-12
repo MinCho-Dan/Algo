b4134316dc66e2118f7be28f7ad46823e5e7a919
gyujik choi <safre2@naver.com>
2025-11-12 17:52:50 +0900

    Maraton76 Solved +4

---
+++ algo/src/Maraton76/BJ_24262.java
@@ -0,0 +1,20 @@
+package Maraton76;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_24262 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(1);
+		System.out.println(0);
+	}
+
+}
+
+
+
