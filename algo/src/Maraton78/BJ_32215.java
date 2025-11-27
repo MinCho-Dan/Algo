4b6e4a7a7bd2bf30eabfc6c74972f434cd9a5360
gyujik choi <safre2@naver.com>
2025-11-27 17:15:51 +0900

    Maraton78 Solved +3

---
+++ algo/src/Maraton78/BJ_32215.java
@@ -0,0 +1,22 @@
+package Maraton78;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BJ_32215 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		if (n-k==0)
+			System.out.println(m*k);
+		else
+			System.out.println(m*(k+1));
+	}
+}
