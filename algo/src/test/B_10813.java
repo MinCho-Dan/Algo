a72d7c9934a712fac0355cd1262a0d800829a0ac
gyujik choi <safre2@naver.com>
2025-12-12 18:37:43 +0900

    BJ 10813 Solved

---
+++ algo/src/test/B_10813.java
@@ -0,0 +1,35 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_10813 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n + 1];
+
+		for (int i = 1; i < n + 1; i++)
+			arr[i] = i;
+
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int tmp = -1;
+			tmp = arr[a];
+			arr[a] = arr[b];
+			arr[b] = tmp;
+		}
+
+		for (int i = 1; i < n + 1; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+}
