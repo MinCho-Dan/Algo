c11a3bb8d7496f1c8f5a2f09ec9d8058578993bc
gyujik <safre2@naver.com>
2025-12-25 20:56:26 +0900

    Maraton82 Solved +2

---
+++ algo/src/Maraton82/BJ_26068.java
@@ -0,0 +1,20 @@
+package Maraton82;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_26068 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int cnt = 0;
+		for (int i = 0; i < n; i++) {
+			String[] tmp = br.readLine().split("-");
+			if (Integer.parseInt(tmp[1]) <= 90)
+				cnt++;
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
