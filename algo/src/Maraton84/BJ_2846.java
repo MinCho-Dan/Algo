67edd13465adb603a434a6f65ef1fc0ff47ccd0c
gyujik <safre2@naver.com>
2026-01-08 19:14:34 +0900

    Maraton84 Solved +1

---
+++ algo/src/Maraton84/BJ_2846.java
@@ -0,0 +1,34 @@
+package Maraton84;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_2846 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int start = -1;
+		int mid = -1;
+		int max = -1;
+		for (int i = 0; i < n; i++) {
+			int current = Integer.parseInt(st.nextToken());
+			if (mid < current) {
+				if (start == -1)
+					start = current;
+				mid = current;
+			} else {
+				max = Math.max(max, mid - start);
+				start = current;
+				mid = current;
+			}
+			if (i == n-1) {
+				max = Math.max(max, mid - start);
+			}
+		}
+
+		System.out.println(max);
+	}
+}
