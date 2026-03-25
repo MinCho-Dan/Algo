989b650b9d3f083001539425629cd4b602f25d2a
gyujik <safre2@naver.com>
2026-03-25 19:45:49 +0900

    Maraton94 Solved +1 (BJ_15624)

---
+++ src/Baekjoon/Silver/BJ_15624.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_15624 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		if (n==0) {
+			System.out.println(0);
+			return;
+		}
+
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+			dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
+		}
+        
+
+        System.out.println(dp[n]);
+    }
+}
