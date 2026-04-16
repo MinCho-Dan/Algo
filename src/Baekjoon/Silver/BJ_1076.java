f3caf3dbaf4de0e06537a458c41a9914d4d895db
gyujik <safre2@naver.com>
2026-04-16 21:37:04 +0900

    Maraton98 Solved +1 (BJ_1076)

---
+++ src/Baekjoon/Silver/BJ_1076.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BJ_1076 {
+	public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        List<Integer>[] graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!bfs(i, graph, N)) {
+                System.out.println("Big World!");
+                return;
+            }
+        }
+
+        System.out.println("Small World!");
+    }
+
+    static boolean bfs(int start, List<Integer>[] graph, int N) {
+        int[] dist = new int[N + 1];
+        Arrays.fill(dist, -1);
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(start);
+        dist[start] = 0;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+
+            for (int next : graph[now]) {
+                if (dist[next] == -1) {
+                    dist[next] = dist[now] + 1;
+                    q.offer(next);
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (dist[i] == -1 || dist[i] > 6) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
