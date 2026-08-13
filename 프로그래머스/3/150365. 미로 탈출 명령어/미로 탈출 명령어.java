f07552004a8cf6e4aed2d450eed42b5b2831c8c2
GyuJik Choi <130440919+MinCho-Dan@users.noreply.github.com>
2026-08-13 14:07:50 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 1.36 ms, Memory: 87.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    int[] dx = {1, 0, 0, -1};
+    int[] dy = {0, -1, 1, 0};
+    char[] dir = {'d', 'l', 'r', 'u'};
+    int n, m, r, c, k;
+
+    public String solution(int n, int m, int x, int y, int r, int c, int k) {
+        this.n = n;
+        this.m = m;
+        this.r = r;
+        this.c = c;
+        this.k = k;
+
+        StringBuilder sb = new StringBuilder();
+
+        return dfs(x, y, sb) ? sb.toString() : "impossible";
+    }
+
+    public boolean dfs(int x, int y, StringBuilder sb) {
+        if (sb.length() == k)
+            return x == r && y == c;
+
+        int remain = k - sb.length();
+        int distance = Math.abs(x - r) + Math.abs(y - c);
+
+        // 가지치기
+        if (remain < distance || (remain - distance) % 2 != 0) return false;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 1 || nx > n || ny < 1 || ny > m) continue;
+
+            sb.append(dir[i]);
+            if (dfs(nx, ny, sb))
+                return true;
+            sb.deleteCharAt(sb.length() - 1);
+        }
+
+        return false;
+    }
+}
